e7e92a79725bafbb5dbcb71aa1f0afb0ff361143
Kyra Henningson <kyrahenningson@hotmail.com>
2023-01-31 18:15:27 -0800

    Kata_33

---
+++ Codewars/8kyu/33_ConvertABooleanToString.ts
@@ -0,0 +1,6 @@
+// Implement a function which convert the
+// given boolean value into its string representation.
+
+export const booleanToString = (b: boolean): string => {
+  return b.toString();
+};
