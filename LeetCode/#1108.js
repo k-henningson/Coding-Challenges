92cab812f800fc1c7c6bb603ecce3267e09aa39c
Kyra Henningson <kyrahenningson@hotmail.com>
2023-02-12 19:34:46 -0800

    #1108

---
+++ LeetCode/#1108.js
@@ -0,0 +1,19 @@
+// Given a valid (IPv4) IP address, return a defanged
+// version of that IP address.
+
+// A defanged IP address replaces every period "." with "[.]".
+
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+
+const defangIPaddr = function (address) {
+  let result = "";
+  for (let i = 0; i < address.length; i++) {
+    if (address[i] === ".") {
+      result += "[.]";
+    } else {
+      result += address[i];
+    }
+  }
+  return result;
+};
