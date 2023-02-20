96a7b46f920199bc67793571783bcf230dae16bf
Kyra Henningson <kyrahenningson@hotmail.com>
2023-02-20 15:12:28 -0800

    CountGoodSubstring

---
+++ NeetCode_150/SlidingWindow/CountGoodSubstrings.ts
@@ -0,0 +1,28 @@
+// A string is good if there are no repeated characters.
+
+// Given a string s​​​​​, return the number of good substrings of length three in s​​​​​​.
+
+// Note that if there are multiple occurrences of the same substring, every occurrence should be counted.
+
+// A substring is a contiguous sequence of characters in a string.
+
+// Input: s = "xyzzaz"
+// Output: 1
+// Explanation: There are 4 substrings of size 3: "xyz", "yzz", "zza", and "zaz".
+// The only good substring of length 3 is "xyz".
+
+// Input: s = "aababcabc"
+// Output: 4
+// Explanation: There are 7 substrings of size 3: "aab", "aba", "bab", "abc", "bca", "cab", and "abc".
+// The good substrings are "abc", "bca", "cab", and "abc".
+
+function countGoodSubstrings2(s: string): number {
+  let count = 0;
+
+  for (let i = 1; i < s.length - 1; i++) {
+    if (s[i] !== s[i - 1] && s[i] !== s[i + 1] && s[i - 1] !== s[i + 1]) {
+      count++;
+    }
+  }
+  return count;
+}
