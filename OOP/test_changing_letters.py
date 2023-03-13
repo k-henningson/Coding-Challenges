01b5e9ae31346e45e2f7a3ac517fa25cabd66b0c
Kyra Henningson <kyrahenningson@hotmail.com>
2023-03-13 12:49:42 -0700

    correct default test statement

--- OOP/test_changing_letters.py
+++ OOP/test_changing_letters.py
@@ -2,8 +2,8 @@ from changing_letters import swap
 
 
 def test_changing_letters():
-    assert swap("HelloWorld!") == "HEllOWOrld!", "Should be HEllOWOrld"
-    assert swap("Hello Peter!") == "HEllO PEtEr!", "Should be HEllO PEtEr"
+    assert swap("HelloWorld!") == "HEllOWOrld!", "Should be HEllOWOrld!"
+    assert swap("Hello Peter!") == "HEllO PEtEr!", "Should be HEllO PEtEr!"
 
 
 if __name__ == "__main__":
