a77b37a94a70cf9a7ac85f190f6689fcc302b64e
Kyra Henningson <kyrahenningson@hotmail.com>
2023-03-16 11:34:29 -0700

    python madlibs game

---
+++ OOP/madlibs/madlibs.py
@@ -0,0 +1,11 @@
+# lunch = ''
+
+# print(f'For lunch today I ate {lunch}')
+
+person = input("Person: ")
+noun = input("Noun: ")
+number = input("Number: ")
+
+
+madlib = f"Pizza was invented by {person}. To make pizza, you need to take a lump of {noun}, cheese, and tomato sauce. Cook in the oven then cut the pizza into {number} slices and enjoy!"
+print(madlib)
