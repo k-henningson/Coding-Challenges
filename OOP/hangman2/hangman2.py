a7ca63a4400e599576a490e671a3641db47d1f6f
peter cresswell <pcresswell@gmail.com>
2023-03-26 01:46:39 +0000

    Make methods and variables private. Add `lives` property.

--- OOP/hangman2/hangman2.py
+++ OOP/hangman2/hangman2.py
@@ -4,56 +4,48 @@ import string
 
 class Hangman:
     def __init__(self, secret_word):
-        self.lives = 6
-        self.secret_word = secret_word.upper()
-        self.alphabet = set(string.ascii_uppercase)
-        self.letters_guessed = set()
+        self._lives = 6
+        self._secret_word = secret_word.upper()
+        self._alphabet = set(string.ascii_uppercase)
+        self._letters_guessed = set()
 
-    def update_secret_word(self):
+    @property
+    def lives(self):
+        """Number of lives remaining"""
+        return self._lives
+
+    def guess(self, letter):
+        """Guesses a letter. Returns the currently solved secret word."""
+        uppercase_letter = letter.upper()
+        if uppercase_letter in self._alphabet:
+            if not uppercase_letter in self._secret_word and not uppercase_letter in self._letters_guessed:
+                self._decrement_lives()
+                if (self._lives == 0):
+                    return self._end_game()
+
+            #  can always add to a set won't cause duplicate
+            self._letters_guessed.add(uppercase_letter)
+            if self._secret_word == self._update_secret_word():
+                return self._win_game()
+
+        return self._update_secret_word()
+
+    def _update_secret_word(self):
         revealed_word = ''
-        for char in self.secret_word:
-            if (char in self.letters_guessed):
+        for char in self._secret_word:
+            if (char in self._letters_guessed):
                 revealed_word += char
             else:
                 revealed_word += '_'
+                
         return revealed_word
 
-    def decrement_lives(self):
-        if (self.lives >= 1):
-            self.lives = self.lives - 1
+    def _decrement_lives(self):
+        if (self._lives >= 1):
+            self._lives = self._lives - 1
 
-    def guess(self, letter):
-        letter = letter.upper()
-        if letter in self.alphabet:
-            if not letter in self.secret_word and not letter in self.letters_guessed:
-                self.decrement_lives()
-                if (self.lives == 0):
-                    return self.end_game()
-            #  can always add to a set won't cause duplicate
-            self.letters_guessed.add(letter)
-            if self.secret_word == self.update_secret_word():
-                return self.win_game()
-        return self.update_secret_word()
+    def _end_game(self):
+        return (f'Game over, the secret word was {self._secret_word}.')
 
-    def end_game(self):
-        return (f'Game over, the secret word was {self.secret_word}.')
-
-    def win_game(self):
-        return (f'Congratulations, you win! The secret word was {self.secret_word}.')
-
-
-game1 = Hangman('hockey')
-print(game1.guess('h'))
-print(game1.guess('o'))
-print(game1.guess('c'))
-print(game1.guess('k'))
-print(game1.guess('e'))
-print(game1.guess('y'))
-
-game2 = Hangman('hockey')
-print(game2.guess('a'))
-print(game2.guess('b'))
-print(game2.guess('d'))
-print(game2.guess('f'))
-print(game2.guess('g'))
-print(game2.guess('i'))
+    def _win_game(self):
+        return (f'Congratulations, you win! The secret word was {self._secret_word}.')
