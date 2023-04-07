e940f49eaee706cf38ed83375883996f8b9a4a47
Kyra Henningson <kyrahenningson@hotmail.com>
2023-04-07 12:15:04 -0700

    Create 8x8 chess Board class and unittests

--- OOP/chess/test_chess.py
+++ OOP/chess/test_chess.py
@@ -3,9 +3,20 @@ from chess import Board
 
 
 class TestChess(unittest.TestCase):
-    def test_display_board_empty_prints_8x8_matrix(self):
+    def test_create_board_prints_8x8_matrix_find_starting_point(self):
         board = Board()
-        self.assertEqual(board.display_board(), "_ _ _ _ _ _ _ _ \n" * 8)
+        self.assertEqual(board._board[0][0], None)
+
+    def test_create_board_prints_8x8_matrix_find_ending_point(self):
+        board = Board()
+        self.assertEqual(board._board[7][7], None)
+
+    def test_create_board_prints_8x8_matrix_find_invalid_point(self):
+        board = Board()
+        with self.assertRaises(IndexError) as context:
+            board._board[8][8]
+        expected_error_msg = "list index out of range"
+        self.assertEqual(str(context.exception), expected_error_msg)
 
 
 if __name__ == '__main__':
