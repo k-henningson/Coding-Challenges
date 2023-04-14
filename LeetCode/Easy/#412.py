183e517e3a22325d9ffe4c6c0a399f64ee90bd24
Kyra Henningson <kyrahenningson@hotmail.com>
2023-04-14 13:46:56 -0700

    #412

---
+++ LeetCode/Easy/#412.py
@@ -0,0 +1,23 @@
+# Fizz Buzz 2 ways in python
+
+for n in range(1, 101):
+    if n % 15 == 0:
+        print('FizzBuzz')
+    else:
+        if n % 3 == 0:
+            print('Fizz')
+        else:
+            if n % 5 == 0:
+                print('Buzz')
+            else:
+                print(n)
+
+for n in range(1, 101):
+    if n % 15 == 0:
+        print('FizzBuzz')
+    elif n % 3 == 0:
+        print('Fizz')
+    elif n % 5 == 0:
+        print('Buzz')
+    else:
+        print(n)
